f5c30af04432253fc7de0feb1d5732e486058b5a
BIPLOV <subedi.biplove10@gmail.com>
2025-01-31 11:59:55 +0545

    Created Controller Based Web Api without mapping Http verbs

---
+++ WebApiDemo/Controllers/ShirtController.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDemo.Controllers
+{
+    [ApiController]
+    public class ShirtController : ControllerBase
+    {
+        public string GetShirts()
+        {
+            return "Reading All the Shirts";
+        }
+        public string GetShirtById(int id)
+        {
+            return $"Reading Shirt with Id {id}";
+        }
+        public string CreateShirt()
+        {
+            return "Creating Shirt";
+        }
+        public string UpdateShirt(int id)
+        {
+            return $"Updating Shirt with Id {id}";
+        }
+        public string DeleteShirt(int id)
+        {
+            return $"Deleting Shirt with Id {id}";
+        }
+
+    }
+}
