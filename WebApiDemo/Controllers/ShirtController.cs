b9488ebb6677a5c2111e60f3acb27b619525f7d5
BIPLOV <subedi.biplove10@gmail.com>
2025-01-31 12:12:39 +0545

    Mapped Different apis With HttpVerbs

--- WebApiDemo/Controllers/ShirtController.cs
+++ WebApiDemo/Controllers/ShirtController.cs
@@ -4,24 +4,31 @@ using Microsoft.AspNetCore.Mvc;
 namespace WebApiDemo.Controllers
 {
     [ApiController]
+    [Route("api/[Controller]")]
     public class ShirtController : ControllerBase
     {
+        [HttpGet]
+       
         public string GetShirts()
         {
             return "Reading All the Shirts";
         }
+        [HttpGet("{id}")]
         public string GetShirtById(int id)
         {
             return $"Reading Shirt with Id {id}";
         }
+        [HttpPost]
         public string CreateShirt()
         {
             return "Creating Shirt";
         }
+        [HttpPut("{id}")]
         public string UpdateShirt(int id)
         {
             return $"Updating Shirt with Id {id}";
         }
+        [HttpDelete("{id}")]
         public string DeleteShirt(int id)
         {
             return $"Deleting Shirt with Id {id}";
--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
@@ -8,25 +9,6 @@ var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/shirts", () =>
-{
-    return "Reading all the shirts";
-});
-app.MapGet("/shirts/{id}", (int id) =>
-{
-    return $"Reading shirt with id : {id}";
-});
-app.MapPost("/shirts", () =>
-{
-    return "Crating Shirt";
-});
-app.MapPut("/shirts/{id}", (int id) =>
-{
-    return $"Updating shirt with id : {id}";
-});
-app.MapDelete("/shirts/{id}", (int id) =>
-{
-    return $"Deleting shirt with id : {id}";
-});
+app.MapControllers();
 app.Run();
 
