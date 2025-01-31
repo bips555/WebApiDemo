58eb7d1a52dca56719edb93f0b2c17a49cf16a8f
BIPLOV <subedi.biplove10@gmail.com>
2025-01-31 11:49:33 +0545

    Add project files.

---
+++ WebApiDemo/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.MapGet("/shirts", () =>
+{
+    return "Reading all the shirts";
+});
+app.MapGet("/shirts/{id}", (int id) =>
+{
+    return $"Reading shirt with id : {id}";
+});
+app.MapPost("/shirts",() =>
+{
+    return "Crating Shirt";
+});
+app.MapPut("/shirts/{id}", (int id) =>
+{
+    return $"Updating shirt with id : {id}";
+});
+app.MapDelete("/shirts/{id}", (int id) =>
+{
+    return $"Deleting shirt with id : {id}";
+});
+app.Run();
+
