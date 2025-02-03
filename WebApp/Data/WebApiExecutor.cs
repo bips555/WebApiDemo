7f7145eaa881b86954362b018a28f250118054a6
BIPLOV <subedi.biplove10@gmail.com>
2025-02-03 14:55:22 +0545

    Error Handling implemented with Custom WebAPi Exception and deserializing jsonError to Error object

--- WebApp/Data/WebApiExecutor.cs
+++ WebApp/Data/WebApiExecutor.cs
@@ -11,27 +11,38 @@
         public async Task<T?> InvokeGet<T>(string relativeUrl)
         {
             var httpClient = _httpClientFactory.CreateClient(apiName);
-            return await httpClient.GetFromJsonAsync<T>(relativeUrl);
+            var request = new HttpRequestMessage(HttpMethod.Get, relativeUrl);
+            var response = await httpClient.SendAsync(request);
+            await HandlePossibleErrors(response);
+            return await response.Content.ReadFromJsonAsync<T>();
         }
 
         public async Task<T?> InvokePost<T>(string relativeUrl, T obj)
         {
             var httpClient = _httpClientFactory.CreateClient(apiName);
             var response = await httpClient.PostAsJsonAsync(relativeUrl, obj);
-            response.EnsureSuccessStatusCode();
+            await HandlePossibleErrors(response);
             return await response.Content.ReadFromJsonAsync<T>();
         }
         public async Task InvokePut<T>(string relativeUrl, T obj)
         {
             var httpClient = _httpClientFactory.CreateClient(apiName);
             var response = await httpClient.PutAsJsonAsync(relativeUrl, obj);
-            response.EnsureSuccessStatusCode();
+            await HandlePossibleErrors(response);
         }
         public async Task InvokeDelete<T>(string relativeUrl)
         {
             var httpClient = _httpClientFactory.CreateClient(apiName);
             var response = await httpClient.DeleteAsync(relativeUrl);
-            response.EnsureSuccessStatusCode();
+            await HandlePossibleErrors(response);
+        }
+        public async Task HandlePossibleErrors(HttpResponseMessage httpResponseMessage)
+        {
+            if (!httpResponseMessage.IsSuccessStatusCode)
+            {
+                var errorJson = await httpResponseMessage.Content.ReadAsStringAsync();
+                throw new WebApiException(errorJson);
+            }
         }
     }
 }
