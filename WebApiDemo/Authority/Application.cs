0bd13d2cf21959af9c8adaabacd197f95c127c46
BIPLOV <subedi.biplove10@gmail.com>
2025-02-04 02:11:46 +0545

    Authority build inside WebApp to authenticate our client app 1st step

---
+++ WebApiDemo/Authority/Application.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApiDemo.Authority
+{
+    public class Application
+    {
+        public int ApplicationId { get; set; }
+        public string? ApplicationName { get; set; }    
+        public string? ClientId { get; set; }
+        public string? Secret { get; set; }
+    }
+}
---
+++ WebApiDemo/Authority/ApplicationRepository.cs
@@ -0,0 +1,26 @@
+﻿namespace WebApiDemo.Authority
+{
+    public static class ApplicationRepository
+    {
+               public static List<Application> _applications = new List<Application>()
+            { 
+                   new Application()
+            {
+                ApplicationId = 1,
+                ApplicationName = "WebApp",
+                ClientId="8A1F3D2B-91C9-4A37-91D1-5C857EE36F19",
+                Secret="7C3A8DF9-5D7B-42F6-9E7E-4A89DB6F8C1B"
+            }
+            };
+        public static bool Authenticate(string clientId,string secret)
+        {
+            return _applications.Any(a => a.ClientId == clientId && a.Secret == secret);
+        }
+        public static Application? GetApplicationByClientId(string clientId)
+        {
+            return _applications.FirstOrDefault(x=>x.ClientId == clientId);
+        }
+        }
+    }
+
+
