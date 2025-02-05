3a4035dcf83b7edcebff673e49cb56c959916f89
BIPLOV <subedi.biplove10@gmail.com>
2025-02-05 12:29:46 +0545

    Added Header Operation for jwt authorization in api endpoints for swaggerGen

--- WebApiDemo/Program.cs
+++ WebApiDemo/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using WebApiDemo.Data;
+using WebApiDemo.Filters.OperationFilter;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -10,7 +12,17 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c=>
+{
+    c.OperationFilter<AuthorizationHeaderOperationFilter>();
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Scheme = "Bearer",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        In= ParameterLocation.Header
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
